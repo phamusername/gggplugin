29dc9af96f6e68f919defd28f9c17b14556b3aad
xemphimhay <lanhp871@gmail.com>
2024-08-06 17:28:59 +0700

    cc

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -10,7 +10,7 @@ version = -1
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem i"
+    description = "Lorem hihi"
     authors = listOf("Cloudburst")
 
     /**
